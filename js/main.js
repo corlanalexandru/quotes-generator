9c183b78ae709506ab0fb2e2ed2538f10953cf64
Alexandru Corlan <alexandru.corlan97@gmail.com>
2017-12-30 15:26:44 +0200

    Added function for random background images and fade effect

--- js/main.js
+++ js/main.js
@@ -4,17 +4,40 @@ $(document).ready(function giveQuote() {
 
     var html = [" "];
     var i = 1;
-    var quoteNumber = Math.floor(Math.random() * (json.length - 0 + 1)) + 0;//Generate an integer for getting a random quote
+    var quoteNumber = Math.floor(Math.random() * (json.length - 1 + 1)) + 1;//Generate an integer for getting a random quote
 
     //Loop through JSON elements to get one of them
     $.each(json, function(key,val){
-      html[i] = '<p><span class="quote-content">' + val.quote + '</span></p>' + '<p>-' + val.author + '-</p>';
+      html[i] = '<div class="hidden"><p><span class="quote-content">' + val.quote + '</span></p>' + '<p>-' + val.author + '-</p></div>';
       i++;
     });
 
-    $(".js-quote").html(html[quoteNumber]);
+    // Add the resulting random quote to js-quote section
+    $(".js-quote").html(html[quoteNumber]);
+    // Fade in effect for quotes
+    $('.hidden').fadeIn(2000);
+    });
 
-  });
-    $("#getQuote").on("click",function(){giveQuote();});
+    $.getJSON("json/background.json",function (json_background){//Get JSON from external source
+
+      var html_background = [" "];
+      var j = 1;
+      var backgroundNumber = Math.floor(Math.random() * (json_background.length - 1 + 1)) + 1;//Generate an integer for getting a random quote
+
+      //Loop through JSON elements to get one of them
+      $.each(json_background, function(key,val){
+        html_background[j] = "'" + val.img_link + "'";
+        j++;
+      });
+
+      //Modify the background-image css property of body
+      $('body').css('background-image','url('+ html_background[backgroundNumber] + ')' );
+    
+    });
+
+   //Call the function on click event
+    $("#getQuote").on("click",function(){
+      giveQuote();
+    });
 
 });
